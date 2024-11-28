f8e20070083ecd3140c01e168d6a3d87cc10b6e2
judiana <yoditfentaw90@gmail.com>
2024-11-28 23:09:19 +0100

    --

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
